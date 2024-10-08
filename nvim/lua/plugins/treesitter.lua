f3ef24c35b4060d269d7267ec55ed9598c04affe
amesaine <amesaine.official@gmail.com>
2024-10-09 00:51:21 +0800

    feat(ts-ctx): changed highlights to normalfloat

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -33,12 +33,8 @@ return {
             require 'treesitter-context'.setup {
                 max_lines = 1,           -- How many lines the window should span. Values <= 0 mean no limit.
                 multiline_threshold = 1, -- Maximum number of lines to show for a single context
-                -- Separator between context and content. Should be a single character string, like '-'.
-                -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-                separator = '-',
-                zindex = 20,     -- The Z-index of the context window
-                on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
             }
+            vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { link = "NormalFloat" })
         end
     }
 }
